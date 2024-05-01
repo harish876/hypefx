2cf40f2d76a02f1c9eef6076ea46cf2e03813e8d
harish876 <harishgokul01@gmail.com>
2024-05-01 17:57:31 +0530

    Changes to the scaffolding template

--- cli/commands/generate/scaffolding/tailwind.config.js
+++ cli/commands/generate/scaffolding/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
     content: [
-      "../**/*.{html,templ,go,js}",
+      // "../**/*.{html,templ,go,js}",
       "./views/**/*/*.{templ,html,js}",
     ],
     theme: {
